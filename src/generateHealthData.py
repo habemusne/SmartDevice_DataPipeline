bb21bec33a4ccca6a74799953ee41b9acc064db8
anshu3769 <37638104+anshu3769@users.noreply.github.com>
2018-09-19 12:47:40 -0400

    Timestamp field added in the data

--- src/generateHealthData.py
+++ src/generateHealthData.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 
 import random
+import datetime
+
 
 def main():
+    now = datetime.datetime.now()
     with open('userLocationData.csv', 'r') as in_file:
         next(in_file)
         with open('deviceGeneartedData.csv', 'w') as out_file:
             for line in in_file:
-                userId = line.split(',')[0]
+                deviceId = line.split(',')[0]
                 zip_code = line.split(',')[1]
                 latitude = line.split(',')[2]
                 longitute = line.split(',')[3]
@@ -15,22 +18,24 @@ def main():
                 
                 num = 70
                 #print(word)
-                if(int(userId) < 20):
+                if(int(deviceId) < 20):
                     num = random.randint(60,70)
 
-                elif(int(userId) < 40):
+                elif(int(deviceId) < 40):
                     num = random.randint(80, 90)
 
-                elif(int(userId) < 60):
+                elif(int(deviceId) < 60):
                     num = random.randint(100, 120)
 
-                elif(int(userId) < 80):
+                elif(int(deviceId) < 80):
                     num = random.randint(80, 90)
 
-                elif(int(userId) <= 100):
+                elif(int(deviceId) <= 100):
                     num = random.randint(40, 50)
 
-                out_file.write(userId + ',' + zip_code + ',' + latitude + ',' + longitute + ',' + city + ',' + str(num) + '\n')
+                out_file.write(deviceId + ',' + zip_code + ',' + latitude + ',' + longitute + ',' + city + ',' \
+                                 + str(now.year) + ',' + str(now.month) + ','  + str(num) + '\n')
+
 
 #Call the main function
 main()
