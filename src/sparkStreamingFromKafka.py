dab5c2df111b81531ce9c58272246c15bfd5b8e8
Anshu Tomar <anshu@Praveens-MacBook-Pro.local>
2018-09-20 13:52:41 -0400

    Code to send streaming data from Kafka producer to spark streaming

--- src/sparkStreamingFromKafka.py
+++ src/sparkStreamingFromKafka.py
@@ -2,11 +2,39 @@ import sys
 from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-from uuid import uuid1
+from pyspark.sql import SparkSession
+
+def writeToRedshift(rdd):
+    #for record in rdd:
+        if(rdd.count() != 0):
+            rdd = rdd.map(lambda x: x.split(",")).map(lambda x: [str(y) for y in x])
+            df = rdd.toDF()
+
+            df.write\
+            .format("com.databricks.spark.redshift")\
+            .option("url", "jdbc:redshift://redshift-cluster-1.cy68m2eaezzl.us-east-1.redshift.amazonaws.com:5439/dev?user=awsuser&password=Awsuser123") \
+            .option("dbtable", "device_data") \
+            .option('forward_spark_s3_credentials',True)\
+            .option("tempdir", "s3n://bucket") \
+            .mode("error") \
+            .save()
+
+
 
 if __name__ == "__main__":
     sc = SparkContext(appName="PythonStreamingRecieverKafka")
-    ssc = StreamingContext(sc, 120) # 120 second window
+    ssc = StreamingContext(sc, 60) # 60 second window
+
+    sqlSpark = SparkSession \
+    .builder \
+    .appName("PythonStreamingRecieverKafka") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+    # hadoop configurations for storing data to redshift
+    sc._jsc.hadoopConfiguration().set("fs.s3n.awsAccessKeyId", "AKIAITVZIRCRA45JISLQ")
+    sc._jsc.hadoopConfiguration().set("fs.s3n.awsSecretAccessKey","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")
+
     broker, topic = sys.argv[1:]
     #inputFromKafka = KafkaUtils.createStream(ssc,broker,"raw-event-streaming-consumer",{topic:1})
 
@@ -16,7 +44,33 @@ if __name__ == "__main__":
     print(inputFromKafka)
 
     lines = inputFromKafka.map(lambda x: x[1])
-    inputFromKafka.saveAsTextFiles("output","csv")
+
+
+  ## Real time processing of the incoming data.
+    ## Get the current location of the users having blood pressure values in abnormal range.
+    ## TO DO : - Check the values against the values you have in your user details and then
+    ## create report based on that comparison.
+    ## Schema of the data is -  DeviceId,Zipcode,Latitude,Longitude,City,Year,Month,Blood Pressure
+
+   # with open('AbnormalDataReport.csv', 'w') as out_file:
+    #    out_file.write("Latitude," + "Longitude,"+ "BloodPressure")
+        #split the data into fields
+        #for line in lines:
+    #    lines.foreachRDD(lambda rdd: rdd.split(","))
+     #       field, count = lines.split(',')
+      #      if(field[8] > 120):
+       #         out_file.write(field[2] + ',' + field[3] + ',' + field[8] + '\n')
+
+
+    ## Save the data for batch processing
+    lines.saveAsTextFiles("output","csv")
+
+
+    ## Write to the Redshift DB
+    #lines.foreachRDD(lambda rdd: rdd.foreachPartition(writeToRedshift))
+    lines.foreachRDD(writeToRedshift)
 
     ssc.start()
     ssc.awaitTermination()
+
+
