c30c40211c52e952d508ee5aad65efa002af53ce
Anshu Tomar <anshu@Praveens-MBP.fios-router.home>
2018-09-29 16:47:42 -0400

    input file paths added as parameters

--- src/sparkJobToGetStreamData.sh
+++ src/sparkJobToGetStreamData.sh
@@ -1,3 +1,4 @@
-#Include kafka, redshift packages and the topic name as spark-submit job parameters
+#Include kafka, redshift packages, topic name  and input file paths as spark-submit job parameters
 
-/usr/local/spark/bin/spark-submit --jars RedshiftJDBC42-no-awssdk-1.2.16.1027.jar --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.0,com.databricks:spark-redshift_2.11:2.0.1,com.databricks:spark-csv_2.11:1.5.0 sparkStreaming.py localhost:9092 part-test
+
+/usr/local/spark/bin/spark-submit --master spark://ec2-18-235-5-9.compute-1.amazonaws.com:7077  --jars RedshiftJDBC42-no-awssdk-1.2.16.1027.jar  --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.1,org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.0,com.databricks:spark-redshift_2.11:2.0.1,com.databricks:spark-csv_2.11:1.5.0 sparkDirectStream.py ec2-18-235-25-194.compute-1.amazonaws.com:9092 device-data /data/gymLocations.csv  /data/userRecords.csv
