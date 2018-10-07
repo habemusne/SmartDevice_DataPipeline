f8b70dda981832a2eadd774c83e7ff5a39baf0b6
anshu3769 <37638104+anshu3769@users.noreply.github.com>
2018-10-07 10:39:08 -0400

    description added

--- src/scripts/create-kafka-topic.sh
+++ src/scripts/create-kafka-topic.sh
@@ -1,1 +1,2 @@
+#Create the topic with replication factor, number of partitions and topic name as arguments to the command
 /usr/local/kafka/bin/kafka-topics.sh --describe --zookeeper localhost:2181 --replication-factor 3 --partitions 3 --topic device-data
