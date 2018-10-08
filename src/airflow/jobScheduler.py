af8450a744df3cfe3a79dfa898ad8274a92ce3a6
Anshu Tomar <anshu@Praveens-MBP.fios-router.home>
2018-10-07 23:09:17 -0400

    created airflow job

---
+++ src/airflow/jobScheduler.py
@@ -0,0 +1,41 @@
+'''
+Schedule job to copy data collected over a day from S3 to 
+Redshift
+'''
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2018, 10, 1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG('Schedule_data_copy_from_S3', default_args=default_args,schedule_interval=timedelta(1))
+
+# t1, t2 and t3 are examples of tasks created by instantiating operators
+t1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag)
+
+t2 = BashOperator(
+    task_id='copy_data_from_s3_to_redshift',
+    bash_command='cd /home/ubuntu;./copyDataFromS3ToRedshift.sh ',
+    retries=3,
+    dag=dag)
+
+
+t2.set_upstream(t1)
